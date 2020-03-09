c60776bc48c8452800b70ad2814d7dc7cd94c827
Martin Wahlmueller <m.wahlmueller@gmx.at>
2020-03-09 20:28:57 +0100

    initial java project setup

---
+++ src/main/java/at/ooe/fr/uwb/em/MainApplication.java
@@ -0,0 +1,11 @@
+package at.ooe.fr.uwb.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
