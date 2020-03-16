fd2413a5aaf8a7738d166dc36feec7269b160149
Martin Wahlmueller <m.wahlmueller@gmx.at>
2020-03-16 20:40:32 +0100

    test if app works

--- src/main/java/at/ooe/fr/uwb/em/controllers/EmController.java
+++ src/main/java/at/ooe/fr/uwb/em/controllers/EmController.java
@@ -1,9 +1,13 @@
 package at.ooe.fr.uwb.em.controllers;
 
+import at.ooe.fr.uwb.em.dtos.TeamDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EmController {
@@ -12,5 +16,15 @@ public class EmController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/teams")
+    public String teams(Model model) {
+        List<TeamDto> teams = new ArrayList<TeamDto>();
+        teams.add(new TeamDto(1, "austria"));
+        teams.add(new TeamDto(2, "italy"));
+        teams.add(new TeamDto(3, "france"));
+        model.addAttribute("teams", teams);
+        return "teams";
     }
 }
